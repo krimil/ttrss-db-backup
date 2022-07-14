3b4846b1091f0dfa34cda7460730b9111e911339
Kristopher Millikan <kristophermillikan@gmail.com>
2022-07-14 18:35:09 -0500

    fix docker-compose command

--- backup-db.sh
+++ backup-db.sh
@@ -2,7 +2,7 @@
 
 cd /ttrss-docker
 source .env
-docker compose exec ttrss-db /bin/bash \
+docker-compose exec ttrss-db /bin/bash \
   -c "export PGPASSWORD=$TTRSS_DB_PASS \
   && pg_dump -U $TTRSS_DB_USER $TTRSS_DB_NAME" \
   | gzip -9 > /backups/backup.sql.gzip
