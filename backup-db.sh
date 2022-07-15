3d73e3121b398c218dba08eb3118a30e5ea8b068
Kristopher Millikan <kristophermillikan@gmail.com>
2022-07-14 19:19:20 -0500

    fix change directory

--- backup-db.sh
+++ backup-db.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-pushd /ttrss-docker
+cd /ttrss-docker
 source .env
 docker-compose exec ttrss-db /bin/bash \
   -c "export PGPASSWORD=$TTRSS_DB_PASS \
   && pg_dump -U $TTRSS_DB_USER $TTRSS_DB_NAME" \
   | gzip -9 > /backups/backup.sql.gzip
-popd
+cd /
 
 BACKUP_DIR=/backups
 find $BACKUP_DIR/* -mtime +$BACKUP_DAYS -exec rm {} \;
