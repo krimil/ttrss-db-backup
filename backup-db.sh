d54d599dc31a6b5069310ca0c25c89a4724c985b
Kristopher Millikan <kristophermillikan@gmail.com>
2022-07-14 20:45:43 -0500

    fix db service name

--- backup-db.sh
+++ backup-db.sh
@@ -2,7 +2,7 @@
 
 cd /ttrss-docker
 source .env
-docker-compose exec ttrss-db /bin/bash \
+docker-compose exec db /bin/bash \
   -c "export PGPASSWORD=$TTRSS_DB_PASS \
   && pg_dump -U $TTRSS_DB_USER $TTRSS_DB_NAME" \
   | gzip -9 > /backups/backup_$(date "+%F-%H%M%S").sql.gzip
